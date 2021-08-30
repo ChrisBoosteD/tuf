d282d100dc772ed9687b20bd07339119179f0cd4
ChrisBoosteD <chris.woon@boostdesign.com.au>
2021-08-30 15:39:23 +1000

    test3

--- tuf/vca.py
+++ tuf/vca.py
@@ -1,2 +1,6 @@
-def test_print():
-    print("hello world")
+class vca:
+    def __init__(self):
+        self.speed = '200'
+    def test_print():
+        print("hello world")
+        print('\t%s ' % speed)
